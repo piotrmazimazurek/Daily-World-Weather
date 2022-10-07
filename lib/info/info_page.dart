99315fde7357684185d55dce1fecf4fbc2b66124
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-10-07 17:49:46 +0200

    Add Second Page

--- lib/info/info_page.dart
+++ lib/info/info_page.dart
@@ -2,20 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 
 class InfoPage extends StatelessWidget {
-  List<PageViewModel> getPages() {
-    return [
-      PageViewModel(
-        image: null,
-        title: 'Strona Testowa',
-        body: 'Wygeneruj Nazwe',
-        footer: const Text('Udanego Straszenia'),
-      ),
-    ];
-  }
-
   const InfoPage({
     Key? key,
   }) : super(key: key);
@@ -27,17 +15,7 @@ class InfoPage extends StatelessWidget {
         title: const Text(
           "Generator Nazwy Potwora Halloween",
         ),
-        backgroundColor: const Color.fromARGB(255, 8, 8, 8),
       ),
-      body: IntroductionScreen(
-          done: Text(
-            'Done',
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-          onDone: () {},
-          pages: getPages()),
     );
   }
 }
