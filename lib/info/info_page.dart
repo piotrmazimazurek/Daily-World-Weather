87ffee9019796d02d3607e1cdda0e1ef23e1af34
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-10-10 16:41:36 +0200

    Delete Unuseful Code

--- lib/info/info_page.dart
+++ lib/info/info_page.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class InfoPage extends StatelessWidget {
   const InfoPage({
@@ -12,10 +11,8 @@ class InfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Generator Nazwy Potwora Halloween",
-        ),
-      ),
+          title: const Text("Za kogo przebierzesz sie na Halloween?"),
+          backgroundColor: const Color.fromARGB(255, 8, 8, 8)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,17 @@
 import 'package:dotestowania/first/first_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  List<PageViewModel> getPages() {
-    return [
-      PageViewModel(
-          image: null,
-          title: 'Strona Testowa',
-          body: 'Wygeneruj Nazwe',
-          footer: Text('Udanego Straszenia'),
-          decoration:
-              PageDecoration(pageColor: Color.fromARGB(255, 153, 59, 30)))
-    ];
-  }
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.orange,
         colorScheme: ColorScheme.fromSwatch(),
