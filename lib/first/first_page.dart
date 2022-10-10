c13aee61ee0cb83c05c0b1be9876ed7b88b025d7
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-10-10 18:11:21 +0200

    Add TextField

--- lib/first/first_page.dart
+++ lib/first/first_page.dart
@@ -1,70 +1,83 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:dotestowania/info/info_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class FirstPage extends StatelessWidget {
-  const FirstPage({
+  FirstPage({
     Key? key,
   }) : super(key: key);
+
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Za kogo przebierzesz sie na Halloween?",
+        appBar: AppBar(
+          title: Text(
+            "Za kogo przebierzesz sie na Halloween?",
+          ),
+          backgroundColor: Color.fromARGB(255, 8, 8, 8),
         ),
-        backgroundColor: const Color.fromARGB(255, 8, 8, 8),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              backgroundImage: AssetImage('images/dynia2.jpg'),
-              radius: 100,
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                '"Dynia or not to Dynia. This is a Halloween Question."- Pułkownik Serious Pumpkin',
-                style: GoogleFonts.lato(
-                  textStyle: Theme.of(context).textTheme.headline4,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
+        body: Center(
+          child: SingleChildScrollView(
+            reverse: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/dynia2.jpg'),
+                  radius: 100,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Nie wiesz za kogo się przebrać ? A może już się przebrałeś i nikt Cię nierozpoznał ? Chcesz obrabować dzieci z cukierków albo mały spożywczak ale wstydzisz się zrobić to w swoim zwykłym wcieleniu? Nie szkodzi ! Generator pomoże podjąć Ci decyzje za kogo się przebrać w Dzień Halloween ! Od teraz w ten dzień będziesz straszyć jako.....',
-                style: GoogleFonts.lato(
-                  textStyle: Theme.of(context).textTheme.headline4,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.italic,
+                SizedBox(
+                  height: 15,
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text(
+                    '"Dynia or not to Dynia. This is a Halloween Question."- Pułkownik Serious Pumpkin',
+                    style: GoogleFonts.lato(
+                      textStyle: Theme.of(context).textTheme.headline4,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Wylosuj swoje przebranie",
+                    style: GoogleFonts.lato(
+                      textStyle: Theme.of(context).textTheme.headline4,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Wpisz swoje imie"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (_) => InfoPage()),
+                    );
+                    controller.clear();
+                  },
+                  child: Text('Wygeneruj'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (_) => const InfoPage()),
-                );
-              },
-              child: const Text('Wygeneruj'),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
