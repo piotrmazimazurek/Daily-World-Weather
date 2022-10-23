8a30a74bbf114a6515e139918057377ae4b6fca7
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-10-23 23:56:20 +0200

    Add REST API

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
 
-class WeatherRepository {
+class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherModel({
     required String city,
   }) async {
-    final response = await Dio().get<Map<String, dynamic>>('');
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=d5724ab8531d4546b8b172321223105&q=London&aqi=yes');
     return response.data;
   }
 }
