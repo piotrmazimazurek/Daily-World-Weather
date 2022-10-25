415d69f332470aaa63bf451ee49d8e6e0a4272fe
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-10-25 14:12:45 +0200

    Add Default Repository

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 
 class WeatherRemoteDataSource {
-  Future<Map<String, dynamic>?> getWeatherModel({
+  Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
     final response = await Dio().get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=d5724ab8531d4546b8b172321223105&q=London&aqi=yes');
+        'http://api.weatherapi.com/v1/current.json?key=d5724ab8531d4546b8b172321223105&q=$city&aqi=yes');
     return response.data;
   }
 }
--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -1,1 +1,23 @@
-class WeatherRepository {}
+import 'dart:convert';
+import 'package:dotestowania/app/domain/models/weather_model.dart';
+import 'package:dotestowania/data/remote_data_sources/weather_remote_data_source.dart';
+
+class WeatherRepository {
+  WeatherRepository(this._weatherRemoteDataSource);
+
+  final WeatherRemoteDataSource _weatherRemoteDataSource;
+
+  Future<WeatherModel?> getWeatherModel({
+    required String city,
+  }) async {
+    final json = await _weatherRemoteDataSource.getWeatherData(
+      city: city,
+    );
+
+    if (json == null) {
+      return null;
+    }
+
+    return WeatherModel.fromJson(json);
+  }
+}
