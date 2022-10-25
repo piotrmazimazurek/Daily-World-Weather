ad1770c465ed648c7e70252d936dacbb0d1aa0ee
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-10-25 14:38:11 +0200

    Add Default State & Cubit Functionality

--- lib/app/first/cubit/first_cubit.dart
+++ lib/app/first/cubit/first_cubit.dart
@@ -1,8 +1,35 @@
 import 'package:bloc/bloc.dart';
+import 'package:dotestowania/app/core/enums.dart';
+import 'package:dotestowania/app/domain/models/weather_model.dart';
+import 'package:dotestowania/repositories/weather_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'first_state.dart';
 
 class FirstCubit extends Cubit<FirstState> {
-  FirstCubit() : super(FirstState());
+  FirstCubit(this._weatherRepository) : super(const FirstState());
+
+  final WeatherRepository _weatherRepository;
+
+  Future<void> getWeatherModel({
+    required String city,
+  }) async {
+    emit(const FirstState(status: Status.loading));
+    try {
+      final weatherModel = await _weatherRepository.getWeatherModel(city: city);
+      emit(
+        FirstState(
+          model: weatherModel,
+          status: Status.success,
+        ),
+      );
+    } catch (error) {
+      emit(
+        FirstState(
+          status: Status.error,
+          errorMessage: error.toString(),
+        ),
+      );
+    }
+  }
 }
--- lib/app/first/cubit/first_state.dart
+++ lib/app/first/cubit/first_state.dart
@@ -1,4 +1,13 @@
 part of 'first_cubit.dart';
 
 @immutable
-class FirstState {}
+class FirstState {
+  const FirstState({
+    this.model,
+    this.status = Status.initial,
+    this.errorMessage,
+  });
+  final WeatherModel? model;
+  final Status status;
+  final String? errorMessage;
+}
