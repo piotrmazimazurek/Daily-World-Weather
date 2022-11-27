13d665440223eeb42923725768b557fac2d85a08
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-11-27 17:04:07 +0100

    Create search & show_weather widgets files

---
+++ lib/app/widgets/search_widget.dart
@@ -0,0 +1,73 @@
+import 'package:dotestowania/app/main/cubit/main_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class SearchWidget extends StatelessWidget {
+  SearchWidget({
+    Key? key,
+  }) : super(key: key);
+
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      reverse: true,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          const CircleAvatar(
+            backgroundImage: AssetImage('images/Moon9.gif'),
+            radius: 65,
+          ),
+          const SizedBox(
+            height: 22,
+          ),
+          AnimatedTextKit(
+            isRepeatingAnimation: true,
+            repeatForever: true,
+            animatedTexts: [
+              TyperAnimatedText('Check Weather',
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    backgroundColor: Colors.black,
+                  )),
+              TyperAnimatedText('And Air Quality',
+                  textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      backgroundColor: Colors.black)),
+            ],
+            onTap: () {
+              ("I am executing");
+            },
+          ),
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), hintText: "Enter Location"),
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+              onPressed: () {
+                context
+                    .read<MainCubit>()
+                    .getWeatherModel(city: _controller.text);
+                _controller.clear();
+              },
+              child: const Text(
+                'Check',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/app/widgets/show_weather_widget.dart
@@ -0,0 +1,55 @@
+import 'package:dotestowania/app/domain/models/weather_model.dart';
+import 'package:dotestowania/app/main/cubit/main_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ShowWeatherWidget extends StatelessWidget {
+  const ShowWeatherWidget({
+    Key? key,
+    required this.weatherModel,
+  }) : super(key: key);
+
+  final WeatherModel weatherModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MainCubit, MainState>(
+      builder: (context, state) {
+        return SingleChildScrollView(
+            reverse: true,
+            child: Column(
+              children: [
+                Text('Temperature:',
+                    style: Theme.of(context).textTheme.subtitle2),
+                Text(
+                  weatherModel.temperature.toString(),
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text('City:', style: Theme.of(context).textTheme.subtitle2),
+                Text(
+                  weatherModel.city,
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+                const SizedBox(height: 20),
+                Text('Condition:',
+                    style: Theme.of(context).textTheme.subtitle1),
+                Text(
+                  weatherModel.condition.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                const SizedBox(height: 20),
+                Text('Air Quality (1-6 scale/Higher = Unhealthy):',
+                    style: Theme.of(context).textTheme.subtitle1),
+                Text(
+                  weatherModel.airquality.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
+            ));
+      },
+    );
+  }
+}
