9dbf080c0b3d3f85b99c5b964288e28f4b28f81f
Piotr Mazurek <pjmaziofficial@gmail.com>
2022-12-27 18:49:28 +0100

    Add icon to weather_model

--- lib/app/domain/models/weather_model.dart
+++ lib/app/domain/models/weather_model.dart
@@ -1,17 +1,20 @@
 class WeatherModel {
   const WeatherModel(
-      {required this.temperature,
+      {required this.icon,
+      required this.temperature,
       required this.city,
       required this.condition,
       required this.airquality});
 
+  final String icon;
   final double temperature;
   final String city;
   final String condition;
   final int airquality;
 
   WeatherModel.fromJson(Map<String, dynamic> json)
-      : temperature = json['current']['temp_c'] + 0.0,
+      : icon = json['current']['condition']['icon'],
+        temperature = json['current']['temp_c'] + 0.0,
         city = json['location']['name'],
         condition = json['current']['condition']['text'],
         airquality = json['current']['air_quality']['us-epa-index'];
