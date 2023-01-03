529d5192fb9e70cd459d547b32fcb5af53704261
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-01-03 18:08:07 +0100

    Add icon_url to weather_model

--- lib/app/domain/models/weather_model.dart
+++ lib/app/domain/models/weather_model.dart
@@ -1,19 +1,21 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings, non_constant_identifier_names
+
 class WeatherModel {
   const WeatherModel(
-      {required this.icon,
+      {required this.icon_url,
       required this.temperature,
       required this.city,
       required this.condition,
       required this.airquality});
 
-  final String icon;
+  final String icon_url;
   final double temperature;
   final String city;
   final String condition;
   final int airquality;
 
   WeatherModel.fromJson(Map<String, dynamic> json)
-      : icon = json['current']['condition']['code'],
+      : icon_url = "http:" + json['current']['condition']['icon'],
         temperature = json['current']['temp_c'] + 0.0,
         city = json['location']['name'],
         condition = json['current']['condition']['text'],
