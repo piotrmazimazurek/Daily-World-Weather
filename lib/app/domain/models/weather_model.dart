feef898a5d4db0a36af95c69e48160620ac2140f
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-01-12 07:03:18 +0100

    Add airquality, localtime & country to weather_model

--- lib/app/domain/models/weather_model.dart
+++ lib/app/domain/models/weather_model.dart
@@ -6,18 +6,27 @@ class WeatherModel {
       required this.temperature,
       required this.city,
       required this.condition,
-      required this.pressure});
+      required this.pressure,
+      required this.airquality,
+      required this.localtime,
+      required this.country});
 
   final String icon_url;
   final double temperature;
   final String city;
   final String condition;
   final double pressure;
+  final int airquality;
+  final String localtime;
+  final String country;
 
   WeatherModel.fromJson(Map<String, dynamic> json)
       : icon_url = "http:" + json['current']['condition']['icon'],
         temperature = json['current']['temp_c'] + 0.0,
         city = json['location']['name'],
         condition = json['current']['condition']['text'],
-        pressure = json['current']['pressure_mb'];
+        airquality = json['current']['air_quality']['us-epa-index'],
+        pressure = json['current']['pressure_mb'],
+        localtime = json['location']['localtime'],
+        country = json['location']['country'];
 }
