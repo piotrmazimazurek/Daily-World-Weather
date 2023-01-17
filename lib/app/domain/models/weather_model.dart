29740ab26e48699dcc8d55e3469d4ad0a7a9e992
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-01-17 18:12:12 +0100

    Divide temperature into f & c in weather_model

--- lib/app/domain/models/weather_model.dart
+++ lib/app/domain/models/weather_model.dart
@@ -3,7 +3,8 @@
 class WeatherModel {
   const WeatherModel(
       {required this.icon_url,
-      required this.temperature,
+      required this.temperature_c,
+      required this.temperature_f,
       required this.city,
       required this.condition,
       required this.pressure,
@@ -12,7 +13,8 @@ class WeatherModel {
       required this.country});
 
   final String icon_url;
-  final double temperature;
+  final double temperature_c;
+  final double temperature_f;
   final String city;
   final String condition;
   final double pressure;
@@ -22,7 +24,8 @@ class WeatherModel {
 
   WeatherModel.fromJson(Map<String, dynamic> json)
       : icon_url = "http:" + json['current']['condition']['icon'],
-        temperature = json['current']['temp_c'] + 0.0,
+        temperature_c = json['current']['temp_c'] + 0.0,
+        temperature_f = json['current']['temp_f'] + 0.0,
         city = json['location']['name'],
         condition = json['current']['condition']['text'],
         airquality = json['current']['air_quality']['us-epa-index'],
