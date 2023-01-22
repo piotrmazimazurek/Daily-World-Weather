086a63a8236f78556be79132e1c3d5536ec14dcf
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-01-22 18:12:35 +0100

    Add response.statuscode in weather_remote_data_source

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -7,8 +7,13 @@ class WeatherRemoteDataSource {
     try {
       final response = await Dio().get<Map<String, dynamic>>(
           'http://api.weatherapi.com/v1/forecast.json?key=5d3b36a454774f5483d124903231001&q=$city&days=7&aqi=yes&alerts=no');
-      (response.data.toString());
-      return response.data;
+      if (response.statusCode == 200) {
+        print(response.data);
+        (response.data.toString());
+        return response.data;
+      } else {
+        throw Exception("Error: ${response.statusCode}");
+      }
     } on DioError catch (error) {
       throw Exception(
           error.response?.data['error']['message'] ?? 'Unknown error');
