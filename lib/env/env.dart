b3c1b16344dfa1953176f813c1c71deb65a2537f
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-02-10 19:37:36 +0100

    Create functioning data in file env.dart

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,1 +1,9 @@
+import 'package:envied/envied.dart';
 
+part 'env.g.dart';
+
+@Envied(path: 'lock.env')
+abstract class Env {
+  @EnviedField(varName: 'key1', obfuscate: true)
+  static final key1 = _Env.key1;
+}
