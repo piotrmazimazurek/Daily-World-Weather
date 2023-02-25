3ffbe646250d6377bca3b298e87d51e4d7ff9ba1
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-02-25 19:56:55 +0100

    Add hive theme box to main page

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@
 import 'package:dotestowania/app.dart';
 import 'package:dotestowania/app/core/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+const themeBox = 'hiveThemeBox';
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(themeBox);
   configureDependencies();
   runApp(const MyApp());
 }
