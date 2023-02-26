e290c9327b8b1bec5fe3a046dca4527e62ad21f9
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-02-26 18:11:22 +0100

    Add value listenable builder to app page

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,33 @@
 import 'package:dotestowania/app/home/home_page.dart';
+import 'package:dotestowania/main.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(),
-        textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme.apply(
-            bodyColor: const Color.fromARGB(255, 255, 255, 255),
-            displayColor: const Color.fromARGB(255, 255, 255, 255))),
-      ),
-      home: const HomePage(),
-    );
+    return ValueListenableBuilder(
+        valueListenable: Hive.box(themeBox).listenable(),
+        builder: (context, box, widget) {
+          //saving the value inside the hive box,
+          var darkMode =
+              Hive.box(themeBox).get('darkMode', defaultValue: false);
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: darkMode ? ThemeMode.dark : ThemeMode.light,
+            darkTheme: ThemeData(
+              colorScheme: ColorScheme.fromSwatch(),
+              textTheme: GoogleFonts.latoTextTheme(Theme.of(context)
+                  .textTheme
+                  .apply(
+                      bodyColor: const Color.fromARGB(255, 255, 255, 255),
+                      displayColor: const Color.fromARGB(255, 255, 255, 255))),
+            ),
+            home: HomePage(value: darkMode),
+          );
+        });
   }
 }
