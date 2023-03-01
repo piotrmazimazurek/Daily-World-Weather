75b2bc835cfa99f89fed04de3ac13b27b6ed6fa8
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-01 20:41:03 +0100

    Rename hive box on main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:dotestowania/app.dart';
 import 'package:dotestowania/app/core/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-const themeBox = 'hiveThemeBox';
+const darkModeBox = 'hiveThemeBox';
 void main() async {
   await Hive.initFlutter();
-  await Hive.openBox(themeBox);
+  await Hive.openBox(darkModeBox);
   configureDependencies();
   runApp(const MyApp());
 }
