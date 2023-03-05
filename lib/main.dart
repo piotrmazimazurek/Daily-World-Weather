0e77332eb784c589dff4539e0640c25abd814d58
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-05 16:55:41 +0100

    Remove hive from main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:dotestowania/app.dart';
 import 'package:dotestowania/app/core/injection_container.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async {
-  await Hive.initFlutter();
-  await Hive.openBox('switchbox');
-
+void main() {
   configureDependencies();
   runApp(const MyApp());
 }
