5301481913b28a46171c4ad1dcce4c04f2991ca5
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-05 16:57:05 +0100

    Create custom light theme data page

---
+++ lib/data/custom_theme_data/custom_light_theme_data.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomLightThemeData {
+  static Color textColor = Colors.black87;
+  static Color appBarColor = const Color.fromARGB(197, 255, 255, 255);
+  static Color scaffoldBgcolor = const Color.fromARGB(169, 255, 255, 255);
+}
