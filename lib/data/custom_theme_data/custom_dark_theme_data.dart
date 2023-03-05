3a34edffeccce8edc471b59a5148a3b5242e27c4
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-05 16:56:57 +0100

    Create custom dark theme data page

---
+++ lib/data/custom_theme_data/custom_dark_theme_data.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomDarkThemeData {
+  static Color textColor = const Color.fromARGB(255, 255, 255, 255);
+  static Color appBarColor = Colors.black87;
+  static Color scaffoldBgcolor = Colors.transparent;
+}
