45f678ddff77e8d58f9c0e413ebba900049249bc
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-07 17:58:01 +0100

    Implement change notifier provider to main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:dotestowania/app.dart';
 import 'package:dotestowania/app/core/injection_container.dart';
+import 'package:dotestowania/app/domain/models/switch_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   configureDependencies();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider.value(
+      value: SwitchModel(),
+      child: const MyApp(),
+    ),
+  );
 }
