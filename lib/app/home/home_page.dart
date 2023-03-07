1df571df1a492a4c0277be86431bc71f81428e66
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-07 17:59:11 +0100

    Implement provider's consumer to home_page flutter switch

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:dotestowania/app/core/enums.dart';
 import 'package:dotestowania/app/core/injection_container.dart';
+import 'package:dotestowania/app/domain/models/switch_model.dart';
 import 'package:dotestowania/app/features/forecast_weather_page/forecast_weather_page.dart';
 import 'package:dotestowania/app/home/cubit/home_cubit.dart';
 import 'package:dotestowania/app/features/search_page/search_page.dart';
@@ -7,6 +8,7 @@ import 'package:dotestowania/app/features/current_weather_page/current_weather_p
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_switch/flutter_switch.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -21,8 +23,6 @@ class _HomePageState extends State<HomePage> {
   Color textColor = const Color.fromARGB(255, 255, 255, 255);
   Color appBarColor = Colors.black87;
   Color scaffoldBgcolor = Colors.transparent;
-
-  bool isSwitchOn = true;
 
   @override
   Widget build(BuildContext context) {
@@ -64,57 +64,59 @@ class _HomePageState extends State<HomePage> {
                     backgroundColor: appBarColor,
                     automaticallyImplyLeading: false,
                     actions: [
-                      FlutterSwitch(
-                        showOnOff: true,
-                        width: 90.0,
-                        height: 50.0,
-                        toggleSize: 45.0,
-                        value: isSwitchOn,
-                        borderRadius: 30.0,
-                        padding: 2.0,
-                        inactiveTextColor: Colors.black,
-                        activeTextColor: Colors.white,
-                        activeTextFontWeight: FontWeight.w400,
-                        inactiveTextFontWeight: FontWeight.w400,
-                        activeToggleColor:
-                            const Color.fromARGB(255, 117, 112, 112),
-                        inactiveToggleColor: const Color(0xFF2F363D),
-                        activeSwitchBorder: Border.all(
-                          color: const Color.fromARGB(188, 117, 112, 112),
-                          width: 6.0,
-                        ),
-                        inactiveSwitchBorder: Border.all(
-                          color: const Color(0xFFD1D5DA),
-                          width: 6.0,
-                        ),
-                        activeColor: const Color.fromARGB(255, 10, 10, 10),
-                        inactiveColor: Colors.white,
-                        activeIcon: const Icon(
-                          Icons.nightlight_round,
-                          color: Color(0xFFF8E3A1),
-                        ),
-                        inactiveIcon: const Icon(
-                          Icons.wb_sunny,
-                          color: Color(0xFFFFDF5D),
-                        ),
-                        onToggle: (value) {
-                          setState(() {
-                            isSwitchOn = value;
+                      Consumer<SwitchModel>(builder: (context, value, _) {
+                        return FlutterSwitch(
+                          showOnOff: true,
+                          width: 90.0,
+                          height: 50.0,
+                          toggleSize: 45.0,
+                          value: value.isSwitchOn,
+                          borderRadius: 30.0,
+                          padding: 2.0,
+                          inactiveTextColor: Colors.black,
+                          activeTextColor: Colors.white,
+                          activeTextFontWeight: FontWeight.w400,
+                          inactiveTextFontWeight: FontWeight.w400,
+                          activeToggleColor:
+                              const Color.fromARGB(255, 117, 112, 112),
+                          inactiveToggleColor: const Color(0xFF2F363D),
+                          activeSwitchBorder: Border.all(
+                            color: const Color.fromARGB(188, 117, 112, 112),
+                            width: 6.0,
+                          ),
+                          inactiveSwitchBorder: Border.all(
+                            color: const Color(0xFFD1D5DA),
+                            width: 6.0,
+                          ),
+                          activeColor: const Color.fromARGB(255, 10, 10, 10),
+                          inactiveColor: Colors.white,
+                          activeIcon: const Icon(
+                            Icons.nightlight_round,
+                            color: Color(0xFFF8E3A1),
+                          ),
+                          inactiveIcon: const Icon(
+                            Icons.wb_sunny,
+                            color: Color(0xFFFFDF5D),
+                          ),
+                          onToggle: (val) {
+                            setState(() {
+                              value.isSwitchOn = val;
 
-                            if (value) {
-                              textColor = Colors.white;
-                              appBarColor = Colors.black87;
-                              scaffoldBgcolor = Colors.transparent;
-                            } else {
-                              textColor = Colors.black87;
-                              appBarColor =
-                                  const Color.fromARGB(197, 255, 255, 255);
-                              scaffoldBgcolor =
-                                  const Color.fromARGB(169, 255, 255, 255);
-                            }
-                          });
-                        },
-                      )
+                              if (val) {
+                                textColor = Colors.white;
+                                appBarColor = Colors.black87;
+                                scaffoldBgcolor = Colors.transparent;
+                              } else {
+                                textColor = Colors.black87;
+                                appBarColor =
+                                    const Color.fromARGB(197, 255, 255, 255);
+                                scaffoldBgcolor =
+                                    const Color.fromARGB(169, 255, 255, 255);
+                              }
+                            });
+                          },
+                        );
+                      })
                     ],
                   ),
                   body: Center(child: Builder(builder: (context) {
