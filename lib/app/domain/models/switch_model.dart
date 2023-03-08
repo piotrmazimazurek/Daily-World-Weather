9cf88e6fc708233df3d0a203d6b75f9cebee3749
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-08 10:09:25 +0100

    Apply color change values as properties logic to switch model

--- lib/app/domain/models/switch_model.dart
+++ lib/app/domain/models/switch_model.dart
@@ -2,10 +2,30 @@ import 'package:flutter/material.dart';
 
 class SwitchModel extends ChangeNotifier {
   bool _isSwitchOn = true;
+  Color _textColor = Colors.white;
+  Color _appBarColor = Colors.black87;
+  Color _scaffoldBgcolor = Colors.transparent;
 
   bool get isSwitchOn => _isSwitchOn;
   set isSwitchOn(bool value) {
     _isSwitchOn = value;
+    _updateColors();
     notifyListeners();
+  }
+
+  Color get textColor => _textColor;
+  Color get appBarColor => _appBarColor;
+  Color get scaffoldBgcolor => _scaffoldBgcolor;
+
+  void _updateColors() {
+    if (_isSwitchOn) {
+      _textColor = Colors.white;
+      _appBarColor = Colors.black87;
+      _scaffoldBgcolor = Colors.transparent;
+    } else {
+      _textColor = Colors.black87;
+      _appBarColor = const Color.fromARGB(197, 255, 255, 255);
+      _scaffoldBgcolor = const Color.fromARGB(169, 255, 255, 255);
+    }
   }
 }
