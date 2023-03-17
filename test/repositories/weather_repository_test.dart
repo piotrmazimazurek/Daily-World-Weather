2420c085a8ad0008741c82adf74b6dee50947fdb
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-17 20:37:22 +0100

    Add null test to weather_repository_test

--- test/repositories/weather_repository_test.dart
+++ test/repositories/weather_repository_test.dart
@@ -6,11 +6,22 @@ import 'package:mocktail/mocktail.dart';
 class MockWeatherDataSource extends Mock implements WeatherRemoteDataSource {}
 
 void main() {
-  WeatherRepository sut;
-  MockWeatherDataSource dataSource;
+  late WeatherRepository sut;
+  late MockWeatherDataSource dataSource;
 
   setUp(() {
     dataSource = MockWeatherDataSource();
     sut = WeatherRepository(weatherRemoteDataSource: dataSource);
+  });
+
+  group('getWeatherModel', () {
+    test('should return null when weather data is null', () async {
+      when(() => dataSource.getWeatherData(city: 'city'))
+          .thenAnswer((_) async => null);
+
+      final results = await sut.getWeatherModel(city: 'city');
+
+      expect(results, isNull);
+    });
   });
 }
