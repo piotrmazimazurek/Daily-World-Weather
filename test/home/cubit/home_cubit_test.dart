d77e3adf29debbf4d303bd7fffa9a532c5fc3a71
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-18 02:03:14 +0100

    Complete home_cubit_test

--- test/home/cubit/home_cubit_test.dart
+++ test/home/cubit/home_cubit_test.dart
@@ -1,7 +1,12 @@
+import 'package:dotestowania/app/core/enums.dart';
+import 'package:dotestowania/app/domain/models/weather_model.dart';
 import 'package:dotestowania/app/home/cubit/home_cubit.dart';
 import 'package:dotestowania/repositories/weather_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+import '../../test_helpers/sample_weather_model.dart';
 
 class MockWeatherRepository extends Mock implements WeatherRepository {}
 
@@ -12,5 +17,29 @@ void main() {
   setUp(() {
     repository = MockWeatherRepository();
     sut = HomeCubit(weatherRepository: repository);
+  });
+
+  group('getWeatherModel', () {
+    WeatherModel weatherModel = getSampleWeatherModel();
+
+    setUp(() => {
+          when(() => repository.getWeatherModel(city: 'city'))
+              .thenAnswer((_) async => Future.value(getSampleWeatherModel()))
+        });
+
+    blocTest<HomeCubit, HomeState>(
+      'emits [MyState] when MyEvent is added.',
+      build: () => sut,
+      act: (cubit) => cubit.getWeatherModel(city: 'city'),
+      expect: () => [
+        const HomeState(
+          status: Status.loading,
+        ),
+        HomeState(
+          status: Status.success,
+          model: weatherModel,
+        ),
+      ],
+    );
   });
 }
