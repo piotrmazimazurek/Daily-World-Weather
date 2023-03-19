32eb6d2ef65d8c46ae1524623712992e28fd3ce2
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-19 19:13:55 +0100

    Add new test which is temporarily skiped to weather_repository_test

--- test/repositories/weather_repository_test.dart
+++ test/repositories/weather_repository_test.dart
@@ -1,7 +1,10 @@
+import 'package:dotestowania/app/domain/models/weather_model.dart';
 import 'package:dotestowania/data/remote_data_sources/weather_remote_data_source.dart';
 import 'package:dotestowania/repositories/weather_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+
+import '../helpers/sample_weather_model.dart';
 
 class MockWeatherDataSource extends Mock implements WeatherRemoteDataSource {}
 
@@ -15,6 +18,19 @@ void main() {
   });
 
   group('getWeatherModel', () {
+    test(
+        'Should return data source with specific weather data depended on what city user provides',
+        () async {
+      when(() => dataSource.getWeatherData(city: 'city'))
+          .thenAnswer((_) async => sampleweatherData);
+
+      final expectedWeatherModel = getSampleWeatherModel();
+
+      final results = await sut.getWeatherModel(city: 'city');
+
+      expect(results, equals(expectedWeatherModel), skip: true);
+    });
+
     test('should return null when weather data is null', () async {
       when(() => dataSource.getWeatherData(city: 'city'))
           .thenAnswer((_) async => null);
