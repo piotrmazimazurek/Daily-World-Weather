0df5c79fccb390b2a9b576ee39e62bccbff15285
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-28 11:33:49 +0200

    Remove device preview from app page cause of aab bundle file update

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:dotestowania/app/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,9 +9,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
       themeMode: ThemeMode.system,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(),
