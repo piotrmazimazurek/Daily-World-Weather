429e642c04eff6b496fa260aa651a945abd2548a
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-28 11:33:24 +0200

    Remove device preview from main page cause of aab bundle file update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:dotestowania/app.dart';
 import 'package:dotestowania/app/core/injection_container.dart';
 import 'package:dotestowania/app/domain/models/switch_model.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   configureDependencies();
-  runApp(DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => ChangeNotifierProvider(
-      create: (context) => SwitchModel(),
+  runApp(
+    ChangeNotifierProvider.value(
+      value: SwitchModel(),
       child: const MyApp(),
     ),
-  ));
+  );
 }
