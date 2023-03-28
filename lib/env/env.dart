3836538447de5fb15f05159e13a9156b9fa525c6
Piotr Mazurek <pjmaziofficial@gmail.com>
2023-03-28 11:37:34 +0200

    Remove env package and files from app

--- lib/env/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: 'locks.env')
-abstract class Env {
-  @EnviedField(varName: 'key1', obfuscate: true)
-  static final key1 = _Env.key1;
-}
